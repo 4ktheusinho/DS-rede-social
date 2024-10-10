047e0f365c133d3c181ea3ef6cfcc6f02641a085
4ktheusinho <126475537+4ktheusinho@users.noreply.github.com>
2024-10-10 14:28:56 -0300

    fix01

--- graficos/common.js
+++ graficos/common.js
@@ -4,10 +4,5 @@ const getCSS = (variavel) => {
   
   export {getCSS}
 
-  const layout = {
-    plot_bgcolor: getCSS('--bg-color'),
-    paper_bgcolor: getCSS('--bg-color')
-    
-}
 
-Plotly.newPlot(grafico, data, layout)
+
